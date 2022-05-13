6111de81ef46f256d1d425b385540662964d4a04
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-05-13 19:28:40 -0300

    TEM QUE COMPLETARKKK

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Touchable } from 'react-native';
+import Cep from './componentes/Cep';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style ={styles.bloco}>
+    <Text style={styles.texto}>Digite seu CEP</Text>
+      
+<TextInput
+placeholder='ex: 11740000'
+keyboardType='numeric'
+style={styles.input}
+/>
+
+<TouchableOpacity
+  style={styles.bloco}
+  >
+    <Text style={styles.txtBloco}>Buscar</Text>
+</TouchableOpacity>
+
+<Cep/>
+      </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bloco:{
+width:'100%',
+alignItems: 'center',
+  },
+
+  texto:{
+    fontSize:20,
+  },
+
+  input:{
+    width:'80%',
+    marginLeft:'5%',
+    borderBottomWidth:1,
+marginTop:20,
+fontsize:30
+  },
+
+  botao:{
+    width:'80%',
+    margintop:30
+  },
+
+  txtBotao:{
+    fontSize:20,
+    textAling:'center'
+  }
 });
